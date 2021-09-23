d11cf416723cc5c734e01ca7e31d542e2e46c333
Olivier Randriamitantsoa <olivier.randriamitantsoa@universite-paris-saclay.fr>
2021-09-23 14:08:03 +0200

    exo00

--- ex00_json2ini/exo00.py
+++ ex00_json2ini/exo00.py
@@ -1,1 +1,18 @@
-import configparser
+import json
+
+with open('sample.json','r') as file:
+    data = json.load(file)
+
+data.keys()
+data.items()
+
+string = ""
+for key in data.keys():
+    string = string + "[" + key + "]\n"
+    for sub_key in data[key].keys():
+        string = string + sub_key + "=" + data[key][sub_key] + "\n"
+    string = string + "\n"
+print(string)
+
+with open('output.ini', 'w') as file:
+    file.write(string)
